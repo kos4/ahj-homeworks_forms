4fa07d12e459dd30060959506c744ae88b5f3e2d
kos4 <kos4.kks@gmail.com>
2024-06-24 23:56:49 +0500

    fix settings

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+/*const CopyWebpackPlugin = require("copy-webpack-plugin");*/
 
 module.exports = {
   target: "web",
@@ -45,13 +45,13 @@ module.exports = {
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
-    new CopyWebpackPlugin({
+    /*new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, "src/static"),
           to: path.resolve(__dirname, "dist"),
         },
       ],
-    }),
+    }),*/
   ],
 };
